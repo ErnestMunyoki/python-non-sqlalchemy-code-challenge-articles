2a4804285424cc3480a083fcd1b2acfc27fe7db3
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-08-16 21:11:52 +0300

    changes made to magazine.py

--- lib/magazine.py
+++ lib/magazine.py
@@ -1,50 +1,58 @@
-from .article import Article
+from article import Article
+from author import Author
 
 class Magazine:
-    all = []
+    _all = []
 
-    def__init__(self, name, category):
-        self.name = name 
+    def __init__(self, name, category):
+        self.name = name
         self.category = category
-        Magazine.all.append(self)
+        Magazine._all.append(self)
 
-@property
-def name(self):
-    return self._name
+    @property
+    def name(self):
+        return self._name
 
-@name.setter 
-def name(self, value):
-    if not isinstance(value, str) or not (2 <= len(value) <= 16):
-        raise Exception("Name must be a string between 2 and 16 chars")
-    self._name = value
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Magazine name must be a string")
+        if not (2 <= len(value) <= 16):
+            raise Exception("Magazine name must be between 2 and 16 characters")
+        self._name = value
 
-@property
-def category(self):
-    return self._category
+    @property
+    def category(self):
+        return self._category
 
-@category.setter
-def category(self, value):
-   if not isinstance(value, str) or len(value.strip()) == 0:
-       raise Exception("Category must be a non-empty string.")
-   self._category = value
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value.strip()) == 0:
+            raise Exception("Magazine category must be a non-empty string")
+        self._category = value
 
-def articles(self):
-    return [article for article in Article.all if article.magazine == self]
+    def articles(self):
+        return [article for article in Article.all() if article.magazine == self]
 
-def contributors(self):
-    return list({article.author for article in self.articles()})
+    def contributors(self):
+        return list(set([article.author for article in self.articles()]))
 
-def article_titles(self):
-    titles = [article.title for article in self.articles()]
-    return titles if titles else None
+    def article_titles(self):
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
-def contribuitng_authors(self):
-    authors = [articles.author for articles in self.articles()]
-    result = [author for author in set(authors) if authors.count(author) > 2]
-    return result if result else None
+    def contributing_authors(self):
+        authors = [article.author for article in self.articles()]
+        frequent = [author for author in set(authors) if authors.count(author) > 2]
+        return frequent if frequent else None
 
-@classmethod
-def top_publisher(cls):
-    if len(Article.all) == 0:
-        return None
-    return max(cls.all, key=lambda mag: len(mag.articles()))
+    @classmethod
+    def top_publisher(cls):
+        if not Article.all():
+            return None
+        return max(cls._all, key=lambda mag: len(mag.articles()))
+
+    @classmethod
+    def all(cls):
+        return cls._all
+
