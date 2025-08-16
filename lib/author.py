b04ca78078fac5146639dcd8e7bc26e1716400a9
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-08-16 21:08:01 +0300

    changes made to author.py

--- lib/author.py
+++ lib/author.py
@@ -1,28 +1,28 @@
-from .article import Article
+from article import Article
+from magazine import Magazine
 
 class Author:
-    def init__(self,name):
+    def __init__(self, name):
         if not isinstance(name, str) or len(name.strip()) == 0:
-            raise Exception("Name must be a non-empty string.")
-            self.name = name
+            raise Exception("Author name must be a non-empty string")
+        self._name = name
 
-@property
-def name(self):
-    return self._name
+    @property
+    def name(self):
+        return self._name
 
-def articles(self):
-    return [articles for articles in Article.all() if articles.author == self]
+    def articles(self):
+        return [article for article in Article.all() if article.author == self]
 
-def magazines(self):
-    return list({article.magazine for article in self.articles()})
+    def magazines(self):
+        return list(set([article.magazine for article in self.articles()]))
 
-def add_article(self, magazine, title):
-    from .magazine import Magazine 
-    if not isinstance(magazine, Magazine):
-        raise Exception("Must provide a Magazine instace.")
-        return Article(magazine, self, title)
+    def add_article(self, magazine, title):
+        if not isinstance(magazine, Magazine):
+            raise Exception("Must provide a Magazine instance")
+        return Article(self, magazine, title)
 
-def topic_areas(self):
-    if len(self.articles()) == 0:
-        return none
-        return list({magazine.category for magazine in self.magazines()})
+    def topic_areas(self):
+        if not self.articles():
+            return None
+        return list(set([magazine.category for magazine in self.magazines()]))
